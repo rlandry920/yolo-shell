8f521b7d5079557eb6bb849a4b9714682a563742
Ryan Landry <rlandry920@gmail.com>
2017-05-01 14:33:56 -0400

    yolo.py: Python script to run darknet detect on user-supplied images.

---
+++ yolo.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+import os
+from PIL import Image
+import matplotlib.pyplot as plt
+
+picture=raw_input('What picture do you want?') 
+
+
+picCount=0;
+all_objects={}
+slices=[]
+pictureName="data/%s.jpg"%picture
+realPicture=Image.open(pictureName)
+picWidth=realPicture.width/3
+ 
+finalImage = Image.new('RGB', (realPicture.width, realPicture.height))
+
+while (picCount<3):
+    realPicture=Image.open(pictureName)
+    
+    croppedPicture = realPicture.crop((picWidth*picCount, 0,picWidth*(picCount+1) ,realPicture.height))
+    croppedPicture.save("data/testing%s.jpg"%picCount)
+    
+ #   croppedPicture.show()
+    
+    os.system("./darknet detect cfg/yolo.cfg yolo.weights data/testing%s.jpg >output.txt"%picCount)
+    img = Image.open('predictions.png')
+    finalImage.paste(img,(picCount*picWidth,0))
+    
+    picCount=picCount+1
+    
+    myFile=open("output.txt","r")
+    for line in myFile:
+        detected_object=line[:line.index(":")]
+        if detected_object in all_objects.keys():
+            all_objects[detected_object]=all_objects[detected_object]+1
+        else:
+            all_objects[detected_object]=1
+    
+    
+
+
+
+finalImage.show()
+for key in all_objects.keys():
+    print key,":",all_objects[key]
+    
+plt.bar(range(len(all_objects)), all_objects.values(), align='center')
+plt.xticks(range(len(all_objects)), all_objects.keys())
+
+plt.show() 
